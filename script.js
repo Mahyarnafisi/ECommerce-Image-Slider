f8c7cc48aa10e439f7e0d4f8981bb63d64d6750e
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-21 14:02:34 +0200

    buttons fucntionallity applied

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+const productContainer = [...document.querySelectorAll(".product__container")];
+
+const btnLeft = [...document.querySelectorAll(".btn--left")];
+const btnRight = [...document.querySelectorAll(".btn--right")];
+
+productContainer.forEach((item, i) => {
+  let containerDimension = item.getBoundingClientRect();
+  let containerWidth = containerDimension.width;
+  btnRight[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  btnLeft[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
